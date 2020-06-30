da5e3e134771bd2b076a00bdbf9405ea58857b58
Giles Thompson <giles@papaiti.com>
2020-06-30 16:20:16 +1200

    Added all packages from ember-cli blueprints.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,46 +1,51 @@
+"use strict";
+
 module.exports = {
   root: true,
+  parser: "babel-eslint",
   parserOptions: {
-    ecmaVersion: 2017,
-    sourceType: 'module'
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
   },
-  plugins: [
-    'ember'
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:ember/recommended'
-  ],
+  plugins: ["ember"],
+  extends: ["eslint:recommended", "plugin:ember/recommended"],
   env: {
-    browser: true
+    browser: true,
   },
   rules: {},
   overrides: [
     // node files
     {
       files: [
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js'
+        ".template-lintrc.js",
+        "ember-cli-build.js",
+        "testem.js",
+        "blueprints/*/index.js",
+        "config/**/*.js",
+        "lib/*/index.js",
       ],
       parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
+        sourceType: "script",
       },
       env: {
         browser: false,
-        node: true
+        node: true,
       },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
-      rules: {
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      }
-    }
-  ]
+      plugins: ["node"],
+      rules: Object.assign(
+        {},
+        require("eslint-plugin-node").configs.recommended.rules,
+        {
+          // add your custom rules and overrides for node files here
+
+          // this can be removed once the following is fixed
+          // https://github.com/mysticatea/eslint-plugin-node/issues/77
+          "node/no-unpublished-require": "off",
+        }
+      ),
+    },
+  ],
 };
